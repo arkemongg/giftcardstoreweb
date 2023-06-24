c586aa4ccee56209aeb48cbbf82e9237e542c4eb
arkemongg <ark.emongg@gmail.com>
2023-06-25 03:57:56 +0600

    changed some design

--- js/urls.js
+++ js/urls.js
@@ -1,2 +1,10 @@
+// Production__url
+
 export const api_url = 'https://web-production-19dd.up.railway.app'
 export const domain_url = 'https://giftcardstoreweb.vercel.app'
+
+
+// Local
+
+// export const api_url = 'http://127.0.0.1:8000'
+// export const domain_url = 'http://127.0.0.1:5500'
